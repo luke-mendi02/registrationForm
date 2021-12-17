403c91cde772d2c99d88b4f02002dd06dab0d759
luke-mendi02 <lukecmendiola@lewisu.edu>
2021-12-17 02:15:19 -0600

    Finished Project

--- registrationForm.js
+++ registrationForm.js
@@ -5,11 +5,8 @@ name:document.getElementById('nameID').value,
 email:document.getElementById('emailID').value,
 password:document.getElementById('passwordID').value
 };
-var oldAccount = {
-
-};
-const addAccount  = (ev) => {
-	ev.preventDefault();
+function addAccount() {
+	
 	let newAccount ={
 		name:document.getElementById('nameID').value,
 		email:document.getElementById('emailID').value,
@@ -18,12 +15,27 @@ const addAccount  = (ev) => {
 	registeredAccountArray.push(newAccount);
 	document.forms[0].reset();
 
-console.warn('added', {registeredAccountArray});
-let pre = document.querySelector('#msg pre');
-pre.textContent = '\n' + JSON.stringify(registeredAccountArray, '\t', 2)
-
+console.warn('added account information', {registeredAccountArray});
 	document.addEventListener('DOMContentLoaded',()=>{
 		document.getElementById('submitSignUp').addEventListener('click', addAccount);
 		
 	});
 };
+
+function passwordStrength() {
+	const indicator = document.querySelector(".indicator");
+	const input = document.querySelector("input");
+	const weak = document.querySelector(".Weak");
+	const decent = document.querySelector(".Decent");
+	const strong = document.querySelector(".Strong");
+	const text = document.querySelector("text");
+	let regE
+	if(input.value !=""){
+		indicator.style.display = "block";
+		indicator.style.display = "flex";
+	}
+	else{
+		indicator.style.display = "none";
+	}
+
+}
